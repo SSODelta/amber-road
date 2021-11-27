5cc4f3cae81259eb22da94b429d44ceb39386484
abadger <viciousbadger@protonmail.com>
2021-11-27 20:02:44 +0100

    Template-generated Vue project
    How to run from command line (requires npm):
    $ cd decom-app
    $ npm run server

---
+++ decom-app/src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="about">
+        <h1>{{ counter }}</h1>
+        <button @click="square()">Square</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: "About",
+    data() {
+        return { counter: 2 }
+    },
+    methods: {
+        square() {
+            this.counter = Math.pow(this.counter, 2);
+        }
+    }
+});
+</script>
