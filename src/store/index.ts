e8c8726aa2b4547345a4f90c339df45cc3006520
abadger <viciousbadger@protonmail.com>
2021-11-30 01:22:17 +0100

    Store pattern w/ type inference

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,51 @@
-import { createStore } from "vuex";
+import { createDirectStore } from "direct-vuex";
 
-export default createStore({
-    state: {},
-    mutations: {},
-    actions: {},
+export interface State {
+    counter: number;
+}
+
+const {
+    store,
+    rootActionContext,
+    moduleActionContext,
+    rootGetterContext,
+    moduleGetterContext
+} = createDirectStore({
+    state: {
+        counter: 2
+    } as State,
+    mutations: {
+        square(state: State) {
+            console.log("square commit boii");
+            state.counter = Math.pow(state.counter, 2);
+        },
+        resetto(state: State) {
+            console.log("square reset boii");
+            state.counter = 2;
+        }
+    },
+    actions: {
+        doubleSquare(context) {
+            const { commit } = rootActionContext(context);
+            commit.square();
+            commit.square();
+        }
+    },
     modules: {},
-});
+})
+
+export default store;
+
+export {
+    rootActionContext,
+    moduleActionContext,
+    rootGetterContext,
+    moduleGetterContext
+}
+
+export type AppStore = typeof store;
+declare module "vuex" {
+    interface Store<S> {
+        direct: AppStore
+    }
+}
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,20 +2,35 @@
     <div class="about">
         <h1>{{ counter }}</h1>
         <button @click="square()">Square</button>
+        &nbsp;
+        <button @click="reset()">Reset</button>
+        &nbsp;
+        <button @click="dblSquare()">DOUBLE SQUARE</button>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import store from "@/store";
+
 export default defineComponent({
     name: "About",
-    data() {
-        return { counter: 2 }
-    },
+    setup() {},
     methods: {
         square() {
-            this.counter = Math.pow(this.counter, 2);
+            store.commit.square();
+        },
+        reset() {
+            store.commit.resetto();
+        },
+        dblSquare() {
+            store.dispatch.doubleSquare();
         }
+    },
+    computed: {
+        counter() {
+            return store.state.counter;
+        },
     }
 });
 </script>
